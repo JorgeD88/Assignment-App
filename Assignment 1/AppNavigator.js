c83d0c5556aa4db81cb300dc66fb93d67356d3f4
JorgeD88 <98435442+JorgeD88@users.noreply.github.com>
2026-03-26 19:09:07 -0500

    Refactor AppNavigator to include task management screens

--- Assignment 1/AppNavigator.js
+++ Assignment 1/AppNavigator.js
@@ -1,17 +1,33 @@
+// navigation/AppNavigator.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/HomeScreen";
+import TaskListScreen from "../screens/TaskListScreen";
 import TaskDetailsScreen from "../screens/TaskDetailsScreen";
+import AddTaskScreen from "../screens/AddTaskScreen";
 
+// Stack navigator: Home (list), Details, Add Task
 const Stack = createNativeStackNavigator();
 
-export default function AppNavigator() {
+export default function AppNavigator({ tasks, setTasks }) {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="TaskDetails" component={TaskDetailsScreen} />
+        <Stack.Screen name="TaskList" options={{ title: "Task Manager" }}>
+          {(props) => (
+            <TaskListScreen {...props} tasks={tasks} setTasks={setTasks} />
+          )}
+        </Stack.Screen>
+        <Stack.Screen
+          name="TaskDetails"
+          component={TaskDetailsScreen}
+          options={{ title: "Task Details" }}
+        />
+        <Stack.Screen name="AddTask">
+          {(props) => (
+            <AddTaskScreen {...props} tasks={tasks} setTasks={setTasks} />
+          )}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
